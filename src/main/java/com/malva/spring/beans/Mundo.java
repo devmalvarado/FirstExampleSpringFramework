94eb09fb059595d6fc3ee0c03c46d374e4cbf2da
dev.malvarado <dev.malvarado@gmail.com>
2020-09-21 01:50:05 -0500

    Anottation Config Class, @Value

--- src/main/java/com/malva/spring/beans/Mundo.java
+++ src/main/java/com/malva/spring/beans/Mundo.java
@@ -1,12 +1,15 @@
 package com.malva.spring.beans;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class Mundo {
-
+	
+	@Value("holi crayolita")
 	private String saludo;
 	
 }
--- src/main/java/com/malva/spring/example/App.java
+++ src/main/java/com/malva/spring/example/App.java
@@ -2,8 +2,9 @@ package com.malva.spring.example;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.malva.spring.beans.AppConfig;
 import com.malva.spring.beans.Mundo;
 
 /**
@@ -13,8 +14,8 @@ import com.malva.spring.beans.Mundo;
 public class App {
     public static void main( String[] args ){
     	
-        ApplicationContext appcontext = new ClassPathXmlApplicationContext("com/malva/spring/xml/beans.xml");
-        Mundo m = (Mundo) appcontext.getBean("mundo");
+        ApplicationContext appcontext = new AnnotationConfigApplicationContext(AppConfig.class);
+    	Mundo m = (Mundo) appcontext.getBean("mundo");
         System.out.println(m.getSaludo());
         ((ConfigurableApplicationContext)appcontext).close();
     }
