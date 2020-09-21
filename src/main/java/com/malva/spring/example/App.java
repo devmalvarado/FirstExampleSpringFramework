c9eff5cdd6e463112f79fd86bb18d173ade97ff2
dev.malvarado <dev.malvarado@gmail.com>
2020-09-21 02:03:23 -0500

    varios appconfig

--- src/main/java/com/malva/spring/example/App.java
+++ src/main/java/com/malva/spring/example/App.java
@@ -5,6 +5,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.malva.spring.beans.AppConfig;
+import com.malva.spring.beans.AppConfig2;
+import com.malva.spring.beans.Marte;
 import com.malva.spring.beans.Mundo;
 
 /**
@@ -14,9 +16,10 @@ import com.malva.spring.beans.Mundo;
 public class App {
     public static void main( String[] args ){
     	
-        ApplicationContext appcontext = new AnnotationConfigApplicationContext(AppConfig.class);
-    	Mundo m = (Mundo) appcontext.getBean("mundo");
-        System.out.println(m.getSaludo());
+        ApplicationContext appcontext = new AnnotationConfigApplicationContext(AppConfig.class, AppConfig2.class);
+    	Marte ma = (Marte) appcontext.getBean("marte");
+    	Mundo mu = (Mundo) appcontext.getBean("mundo");
+        System.out.println(ma.getSaludo() + mu.getSaludo());
         ((ConfigurableApplicationContext)appcontext).close();
     }
 }
