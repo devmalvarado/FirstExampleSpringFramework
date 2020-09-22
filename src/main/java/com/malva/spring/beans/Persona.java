d30d28083cedf90d8251c03d82434f7f2d679a2f
dev.malvarado <dev.malvarado@gmail.com>
2020-09-22 15:17:18 -0500

    postconstruc, predestroy varios objetos

--- src/main/java/com/malva/spring/beans/Persona.java
+++ src/main/java/com/malva/spring/beans/Persona.java
@@ -3,7 +3,8 @@ package com.malva.spring.beans;
 
 import lombok.Getter;
 import lombok.Setter;
-
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Setter
 @Getter
@@ -15,10 +16,11 @@ public class Persona {
 	private Pais pais;
 	private Ciudad ciudadela;
 	
+	@PostConstruct
 	private void initBean() {
 		System.out.println("antes de iniciar el Bean");
 	}
-	
+	@PreDestroy
 	private void destroy() {
 		System.out.println("Bean justo antes de ser destruido");
 	}
